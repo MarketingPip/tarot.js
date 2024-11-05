1f6e14c8a8c04118fa5f43e343d742cbd2bd14d7
Jared Van Valkengoed <86180097+MarketingPip@users.noreply.github.com>
2024-11-04 22:48:59 -0500

    Create license.config.js

---
+++ src/license.config.js
@@ -0,0 +1,22 @@
+/* Stuff for webpack config / build process 
+Note / step for cutting release -
+update package.json FIRST with version - to keep updated version in license.
+THEN create build.
+*/ 
+import packageData from '../package.json' assert { type: 'json' };
+
+
+const REPONAME = `Emoji-Fallback.js`;
+
+const LICENSE = `/**!
+ * @license ${REPONAME} - ${packageData.description}
+ * VERSION: ${packageData.version}
+ * LICENSED UNDER ${packageData?.license} LICENSE
+ * MORE INFO CAN BE FOUND AT https://github.com/MarketingPipeline/${REPONAME}/
+ */`;
+
+const FILENAME = "emoji-fallback"; // used for output file name
+
+const packageCONFIG = {LICENSE,FILENAME};
+
+module.exports = packageCONFIG
